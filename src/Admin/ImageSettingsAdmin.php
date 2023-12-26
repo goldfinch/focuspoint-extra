e172a1e00b75779a18de5b913f74be9549aaa889
Art Karasev <goldfinch@users.noreply.github.com>
2023-12-27 01:27:07 +1300

    missing components

--- src/Admin/ImageSettingsAdmin.php
+++ src/Admin/ImageSettingsAdmin.php
@@ -6,7 +6,7 @@ use SilverStripe\Assets\Image;
 use SilverStripe\Admin\ModelAdmin;
 use SilverStripe\View\Requirements;
 use SilverStripe\Forms\GridField\GridFieldConfig;
-use Goldfinch\Seo\Forms\GridField\MetaEditorFocusPointColumn;
+use Goldfinch\ImageSettings\Forms\GridField\MetaEditorFocusPointColumn;
 use Goldfinch\ImageSettings\Forms\GridField\GridFieldManyManyFocusConfig;
 
 class ImageSettingsAdmin extends ModelAdmin
